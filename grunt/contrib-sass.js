c9ecbaca220cdb6a7da3b81a702847afed7b1e5a
Prawesh <praweshrajkarnikar@gmail.com>
2015-08-27 23:51:42 +0545

    test job

---
+++ grunt/contrib-sass.js
@@ -0,0 +1,23 @@
+/**
+ * Sass - CSS preprocessor (Ruby version)
+ */
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.config('sass', {
+    dist: {
+      options: {
+        style: 'expanded',
+        loadPath: [
+          '<%= xh.src %>/bower_components/',
+          '<%= xh.src %>/bower_components/bootstrap-sass/assets/stylesheets/'
+        ],
+        sourcemap: 'none',
+        unixNewlines: true
+      },
+      files: {
+        '<%= xh.dist %>/css/main.css': '<%= xh.src %>/scss/main.scss'
+      }
+    }
+  });
+};
---
+++ src/js/main.js
@@ -0,0 +1,60 @@
+/* ==========================================================================
+
+    Project: xplosion
+    Author: XHTMLized
+    Last updated: @@timestamp
+
+   ========================================================================== */
+
+(function($) {
+
+  'use strict';
+
+  var App = {
+
+    /**
+     * Init Function
+     */
+    init: function() {
+      App.feature1();
+      // App.feature2();
+    },
+
+    /**
+     * Custom feature 1
+     */
+    feature1: function() {
+      //Initailize colorbox
+      var $gallery = $(".colorbox");
+      $gallery.colorbox({rel:'colorbox', slideshow:true});
+      //Auto trigger colorbox on page load
+      $gallery.first().trigger('click');
+      var $count = $gallery.length;
+      var $flag = 1;
+
+      //Trigger slideshow through event hooks
+      $(document).bind('cbox_complete', function(){
+        setTimeout($.colorbox.next, 250);
+
+        //Auto close slideshow after last slide
+        if($flag == $count) {
+          $.colorbox.close();
+        }
+        $flag = $flag + 1;
+      });
+    },
+
+    /**
+     * Custom feature 2
+     */
+    feature2: function() {
+
+    }
+
+  };
+
+  $(function() {
+    App.init();
+  });
+
+})(jQuery);
